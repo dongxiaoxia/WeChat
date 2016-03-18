6deb49e6f1a2080d66aba80fe1f919f0db32810b
unknown <>
2016-03-18 14:44:18 +0800

    commit

---
+++ src/main/java/xyz/dongxiaoxia/wechat/App.java
@@ -0,0 +1,13 @@
+package xyz.dongxiaoxia.wechat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
